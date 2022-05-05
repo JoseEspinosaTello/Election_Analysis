a72e83ae8881d4fbfbd7c2e0c89532976d1a5c29
JoseEspinosaTello <jet5588@outlook.com>
2022-05-05 14:02:55 -0500

    update

---
+++ Python_practice.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+print(type(" "))
+
+a = 5 + 2 * 3
+
+b = 8 // 5 - 3
+c = 8 + 22 * 2 - 4
+d = 16 - 3 / 2 + 7 - 1
+e = 3 ** 3 % 5
+f = 5 + 9 * 3 / 2 - 4
+
+print(a, b, c, d, e, f)
+
+counties = ["Arapahoe","Denver","Jefferson"]
+
+print(counties)
+
+print(counties[0])
+
+print(len(counties))
