1722bd53d952390052328887cc54dea864555dad
JoseEspinosaTello <jet5588@outlook.com>
2022-05-09 15:37:46 -0500

    update

--- Python_practice.py
+++ Python_practice.py
@@ -19,3 +19,54 @@ print(counties)
 print(counties[0])
 
 print(len(counties))
+
+#3.2.10
+
+for i in range(len(counties)):
+
+	print(counties[i])
+
+counties_tuple = ("Arapahoe","Denver","Jefferson")
+
+for county in counties_tuple:
+	print(county)
+
+#iterate through a DIctionary
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+for county in counties_dict:
+
+	print(county)
+
+for county in counties_dict.keys(): #returns keys
+	print(county)
+
+for voters in counties_dict.values(): #returns values
+    print(voters)
+
+for key, value in counties_dict.items(): #returns key and value
+	print(key,value)
+
+#Skill Drill Print each county and registered voter form the counties dictionary so that the output looks like this:
+
+# Iterate Through a List of Dictionaries
+
+voting_data=[{"county":"Arapahoe","registered_voters":422829},
+		{"county":"Denver","registered_voters":463353},
+{"county":"Jefferson","registered_voters":432438}]
+
+for county_dict in voting_data:
+	print(county_dict)
+
+for i in range(len(voting_data)):
+
+      print(voting_data[i]['county'])
+
+#Get the Values from a List of Dictionaries
+
+for county_dict in voting_data:
+
+	for value in county_dict.values():
+	
+		print(value)
